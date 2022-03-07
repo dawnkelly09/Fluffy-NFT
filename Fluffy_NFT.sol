b7816589e9f3823b54c7f92062a8e7611369190b
Dawn Kelly <83190195+dawnkelly09@users.noreply.github.com>
2022-03-07 12:39:14 -0800

    Refactored, compiles and mints
    Still needs WL, reserve NFTs, payment handling, and other features added.

--- Fluffy_NFT.sol
+++ Fluffy_NFT.sol
@@ -1,187 +1,256 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.0;
 
-/*With thanks to @nftchance, @masonnft, and @nuclearnerds for all of
-the resources they share with developers. This contract draws
-on concepts from Nuclear Nerds and Chance's Medium articles*/
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/interfaces/IERC2981.sol";
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
 
-//imports
-import "./IERC721.sol";
-import "./IERC721Receiver.sol";
-import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./ERC721Enumerable.sol";
-import "./extensions/IERC721Metadata.sol";
-import "../../utils/Address.sol";
-import "../../utils/Context.sol";
-import "../../utils/Strings.sol";
-import "../../utils/introspection/ERC165.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract FluffyNFT is Context, ERC165, IERC721, ERC721Enumerable, Ownable, IERC721Metadata {
-    using Address for address;
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract MuncheezNFT is ERC721, IERC2981, Ownable, ReentrancyGuard {
+    using Counters for Counters.Counter;
     using Strings for uint256;
+    Counters.Counter private tokenCounter;
 
-    //state variables
-    string public               baseURI;
-    address public immutable    proxyRegistryAddress; 
-    uint256 public              MAX_SUPPLY;
-    uint256 public constant     MAX_PER_TX          = 3;
-    uint256 public constant     RESERVES            = 100;
-    uint256 public constant     priceInWei          = 0.05 ether;
+    string  public                  baseURI;
+    string  public                  verificationHash;
+    address private                 openSeaProxyRegistryAddress;
+    bool    private                 isOpenSeaProxyActive = true;
 
-    //constructor
-    constructor(string memory _baseURI,
-                address _proxyRegistryAddress
+    //set at one over desired max per wallet to allow simple < check
+    uint256 public constant         MAX_MUNCHEEZ_PER_WALLET = 3;
+    uint256 public                  maxMuncheez;
+    //same price will be used for WL & Public sales
+    uint256 public constant         NFT_SALE_PRICE = 0.06 ether;
+    bool    public                  isPublicSaleActive;
+    uint256 public                  maxWhitelistSaleMuncheez;
+    bytes32 public                  whitelistSaleMerkleRoot;
+    bool    public                  isWhitelistSaleActive;
 
+    uint256 public                  maxGiftedMuncheez;
+    uint256 public                  numGiftedMuncheez;
+    bytes32 public                  claimListMerkleRoot;
+    address public                  royaltyReceiverAddress;
+
+
+
+    mapping(address => uint256) public whitelistMintCounts;
+    mapping(address => bool) public claimed;
+
+
+
+    //ACCESS CONTROL AND STATE MODIFIERS
+
+    modifier publicSaleActive() {
+        require(isPublicSaleActive, "Public sale is not open");
+        _;
+    }
+
+    modifier whitelistSaleActive() {
+        require(isWhitelistSaleActive, "Whitelist sale is not open");
+        _;
+    }
+
+    modifier isCorrectPayment(uint256 price, uint256 numberOfTokens) {
+        require(
+            price * numberOfTokens == msg.value,
+            "Incorrect ETH value sent"
+        );
+        _;
+    }
+
+    modifier canMintMuncheez(uint256 numberOfTokens) {
+        require(
+            tokenCounter.current() + numberOfTokens <=
+                maxMuncheez - maxGiftedMuncheez,
+            "Not enough Muncheez remaining to mint"
+        );
+        _;
+    }
+
+    modifier maxMuncheezPerWallet(uint256 numberOfTokens) {
+        require(
+            balanceOf(msg.sender) + numberOfTokens <= MAX_MUNCHEEZ_PER_WALLET,
+            "Max Muncheez to mint is two"
+        );
+        _;
+    }
+
+    modifier canGiftMuncheez(uint256 num) {
+        require(
+            numGiftedMuncheez + num <= maxGiftedMuncheez,
+            "Not enough Muncheez remaining to gift"
+        );
+        require(
+            tokenCounter.current() + num <= maxMuncheez,
+            "Not enough Muncheez remaining to mint"
+        );
+        _;
+    }
+
+    
+
+    modifier isValidMerkleProof(bytes32[] calldata merkleProof, bytes32 root) {
+        require(
+            MerkleProof.verify(
+                merkleProof,
+                root,
+                keccak256(abi.encodePacked(msg.sender))
+            ),
+            "Address does not exist in list"
+        );
+        _;
+    }
+
+    constructor(
+        string memory _baseURI,
+        uint256 _maxMuncheez,
+        uint256 
     ) 
-        ERC721("Fluffy Friends", "FLUFF")
+    ERC721("MuncheezNFT", "MUNCH") 
     {
-        _baseURI = baseURI;
-        /*The OpenSea Proxy Registry addresses are:
-Rinkeby: 0xf57b2c51ded3a29e6891aba85459d600256cf317
-Mainnet: 0xa5409ec958c83c3f309868babaca7c86dcb077c1*/
-        _proxyRegistryAddress = proxyRegistryAddress;
+      baseURI = _baseURI;
+      maxMuncheez = _maxMuncheez;
+        
 
-
-    //modifiers
-
-    //onlyOwner functions
-
-    //when whitelist mint is over, call this function and pass desired max supply of NFTs (highest possible token ID)
-    function togglePublicSale(uint256 _MAX_SUPPLY) external onlyOwner {
-        delete whitelistMerkleRoot;
-        MAX_SUPPLY = _MAX_SUPPLY;
-
-    //allow collection of reserved NFTs by owner/team
-    function collectReserves() external onlyOwner {
-        require(_owners.length == 0, 'Reserves already taken.');
-        for(uint256 i; i < RESERVES; i++)
-            _mint(_msgSender(), i);
-
-    //set base URI --> ipfs://<hash>
-    function setBaseURI(string memory _baseURI) public onlyOwner {
-        baseURI = _baseURI;
     }
 
 
-    //functions
+//PUBLIC FUNCTIONS FOR MINTING
 
-    function publicMint(uint256 count) public payable {
-        uint256 totalSupply = _owners.length;
-        require(totalSupply + count < MAX_SUPPLY, "Exceeds max supply.");
-        require(count < MAX_PER_TX, "Exceeds max per transaction.");
-        require(count * priceInWei == msg.value, "Invalid funds provided.");
-    
-        for(uint i; i < count; i++) { 
-            _mint(_msgSender(), totalSupply + i);
+    function mint(uint256 numberOfTokens)
+        external
+        payable
+        nonReentrant
+        isCorrectPayment(NFT_SALE_PRICE, numberOfTokens)
+        publicSaleActive
+        canMintMuncheez(numberOfTokens)
+        maxMuncheezPerWallet(numberOfTokens)
+    {
+        for (uint256 i = 0; i < numberOfTokens; i++) {
+            _safeMint(msg.sender, nextTokenId());
         }
     }
 
-    function tokenURI(uint256 _tokenId) public view override returns (string memory) {
-        require(_exists(_tokenId), "Token does not exist.");
-        return string(abi.encodePacked(baseURI, Strings.toString(_tokenId)));
-    }
-
-
-
-    //required for ERC-721 standard
-
-    function transferFrom(
-        address from,
-        address to,
-        uint256 tokenId
-    ) public virtual override {
-        (_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: transfer caller is not owner nor approved");
-
-        _transfer(from, to, tokenId);
-    }
-    
-    function _safeTransfer(
-        address from,
-        address to,
-        uint256 tokenId,
-        bytes memory _data
-    ) internal virtual {
-        _transfer(from, to, tokenId);
-        require(_checkOnERC721Received(from, to, tokenId, _data), "ERC721: transfer to non ERC721Receiver implementer");
-    }
-
-
-    function safeTransferFrom(
-        address from,
-        address to,
-        uint256 tokenId,
-        bytes memory _data
-    ) public virtual override {
-        require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: transfer caller is not owner nor approved");
-        _safeTransfer(from, to, tokenId, _data);
-    }
-
-    function transferFrom(
-        address from,
-        address to,
-        uint256 tokenId
-    ) public virtual override {
-        //solhint-disable-next-line max-line-length
-        require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: transfer caller is not owner nor approved");
-
-        _transfer(from, to, tokenId);
-    }
-
-    function setApprovalForAll(address operator, bool approved) public virtual override {
-        _setApprovalForAll(_msgSender(), operator, approved);
-
-    function getApproved(uint256 tokenId) public view virtual override returns (address) {
-        require(_exists(tokenId), "ERC721: approved query for nonexistent token");
-
-        return _tokenApprovals[tokenId];
-    }
-    
-    function approve(address to, uint256 tokenId) public virtual override {
-        address owner = ERC721.ownerOf(tokenId);
-        require(to != owner, "ERC721: approval to current owner");
+    function mintWhitelistSale(
+        uint8 numberOfTokens,
+        bytes32[] calldata merkleProof
+    )
+        external
+        payable
+        nonReentrant
+        whitelistSaleActive
+        canMintMuncheez(numberOfTokens)
+        isCorrectPayment(NFT_SALE_PRICE, numberOfTokens)
+        isValidMerkleProof(merkleProof, whitelistSaleMerkleRoot)
+    {
+        uint256 numAlreadyMinted = whitelistMintCounts[msg.sender];
 
         require(
-            _msgSender() == owner || isApprovedForAll(owner, _msgSender()),
-            "ERC721: approve caller is not owner nor approved for all"
+            numAlreadyMinted + numberOfTokens <= MAX_MUNCHEEZ_PER_WALLET,
+            "Max Muncheez to mint in whitelist sale is two"
         );
 
-        _approve(to, tokenId);
+        require(
+            tokenCounter.current() + numberOfTokens <= maxWhitelistSaleMuncheez,
+            "Not enough Muncheez remaining to mint"
+        );
+
+        whitelistMintCounts[msg.sender] = numAlreadyMinted + numberOfTokens;
+
+        for (uint256 i = 0; i < numberOfTokens; i++) {
+            _safeMint(msg.sender, nextTokenId());
+        }
     }
-    
-    function balanceOf(address owner) public view virtual override returns (uint256) {
-        require(owner != address(0), "ERC721: balance query for the zero address");
-        return _balances[owner];
 
-    function ownerOf(uint256 tokenId) public view virtual override returns (address) {
-        address owner = _owners[tokenId];
-        require(owner != address(0), "ERC721: owner query for nonexistent token");
-        return owner;
 
-    //required to interact with imported interfaces
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC165, IERC165) returns (bool) {
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        require(_exists(tokenId), "Nonexistent token");
+
         return
-            interfaceId == type(IERC721).interfaceId ||
-            interfaceId == type(IERC721Metadata).interfaceId ||
-            super.supportsInterface(interfaceId);
-}
-
-
-contract OwnableDelegateProxy { }
-contract OpenSeaProxyRegistry {
-    mapping(address => OwnableDelegateProxy) public proxies;
-    mapping(address => bool) proxyToApproved;
-}   
-
-//allows holders to list on OpenSea with no gas fees
-    function isApprovedForAll(address _owner, address operator) public view override
-returns (bool) {
-    OpenSeaProxyRegistry proxyRegistry = OpenSeaProxyRegistry(proxyRegistryAddress);
-    if (address(proxyRegistry.proxies(_owner)) == operator) return true;
-    return super.isApprovedForAll(_owner, operator);
-}
-//allows gasless future collection approval for cross-collection interaciton
-    function flipProxyState(address proxyAddress) public onlyOwner {
-        proxyToApproved[proxyAddress] =!proxyToApproved[proxyAddress];
+            string(abi.encodePacked(baseURI, "/", tokenId.toString(), ".json"));
     }
+    //needs fixed re: known Crypto Coven royalty issue
+    function royaltyInfo(uint256 tokenId, uint256 salePrice)
+        external
+        view
+        override
+        returns (address receiver, uint256 royaltyAmount)
+    {
+        require(_exists(tokenId), "Nonexistent token");
+
+        return (address(royaltyReceiverAddress), SafeMath.div(SafeMath.mul(salePrice, 5), 100));
+    }
+
+    
+//PUBLIC READ ONLY FUNCTIONS
+
+function getBaseURI() external view returns (string memory) {
+        return baseURI;
+    }
+
+//OWNER ONLY FUNCTIONS
+
+    function setBaseURI(string memory _baseURI) external onlyOwner {
+        baseURI = _baseURI;
+    }
+
+//Enter true if whitelist sale is active, else enter false
+    function setIsWhitelistSaleActive(bool _isWhitelistSaleActive)
+        external
+        onlyOwner
+    {
+        isWhitelistSaleActive = _isWhitelistSaleActive;
+    }
+
+//Enter true if public sale is active, else enter false
+    function setIsPublicSaleActive(bool _isPublicSaleActive)
+        external
+        onlyOwner
+    {
+        isPublicSaleActive = _isPublicSaleActive;
+    }
+
+//owner can mint the next available token to a specified address
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = tokenCounter.current();
+        tokenCounter.increment();
+        _safeMint(to, tokenId);
+    }
+
+   
+
+//SUPPORTING FUNCTIONS
+
+    function nextTokenId() private returns (uint256) {
+        tokenCounter.increment();
+        return tokenCounter.current();
+    }
+
+    
+    // The following functions are overrides required by Solidity.
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC721, IERC165)
+        returns (bool)
+    {
+        return
+            interfaceId == type(IERC2981).interfaceId ||
+            super.supportsInterface(interfaceId);
+    }
+}
